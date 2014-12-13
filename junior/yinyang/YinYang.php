f56f5e6fe4d91038f92504edab672ed1e68e3f78
Olivera Bikić <oljapf@gmail.com>
2014-12-13 15:03:44 +0100

    provera parametara

--- junior/yinyang/YinYang.php
+++ junior/yinyang/YinYang.php
@@ -2,36 +2,38 @@
 
 class YinYang
 {
-	public static function divisionCheck ($int1, $int2){
-		$start=0;
-		$end=0;
+    public static function divisionCheck ($int1, $int2)
+    {        
+        $start = 0;
+        $end =0;
 
-		if ($int1 <= $int2) {
-			$start = $int1;
-			$end = $int2;
-		}
+        if ((is_int($int1)) && (is_int($int2))) {
+            if ($int1 <= $int2) {
+                $start = $int1;
+                $end = $int2;
+            }
 
-		else{
-			$start = $int2;
-			$end = $int1;
-		}
+            else{
+                $start = $int2;
+                $end = $int1;
+            }
 
-		for ($start; $start <= $end; $start++) {
-			if(($start % 3 == 0) && ($start % 5 == 0)){
-				echo 'Yin Yang';
-			}
-		else if ($start % 3 == 0) {
-			echo 'Yin';
-		}
-		else if ($start % 5 == 0){
-			echo 'Yang';
-		}
-		else {
-			echo $start;
-		}
-		echo ' ';
-		}
-	}
+            for ($start; $start <= $end; $start++) {
+                if(($start % 3 == 0) && ($start % 5 == 0)){
+                    echo 'Yin Yang';
+                } else if ($start % 3 == 0) {
+                    echo 'Yin';
+                } else if ($start % 5 == 0){
+                    echo 'Yang';
+                } else {
+                    echo $start;
+                }
+                echo ' ';
+            }
+        } else {
+            echo "He's dead Jim!";
+        }
+    }
 }
 
 YinYang::divisionCheck(2, 17);
