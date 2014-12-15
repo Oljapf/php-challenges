e3a7155dfef46b8c29d7c01a1af03a4a00d5a795
Olivera Bikić <oljapf@gmail.com>
2014-12-15 01:47:46 +0100

    novi pristup, krajnje resenje

--- junior/iso6801/ISO6801.php
+++ junior/iso6801/ISO6801.php
@@ -1,129 +1,21 @@
 <?php
-  
-function solution1 ($date) {
-    $year = substr($date, 3, 2);
-    $output = '-' . substr($date, 0, 2) . '-' . substr($date, 6, 2);
 
-    if($year <= 14){
-        $output = '20' . $year . $output;
-    } else {
-        $output = '19' . $year . $output;
-    }
-
-    return $output;
-}
-
-function solution2 ($date) {
-    $month = substr($date, 0, 3);
-
-    switch ($month) {
-        case 'Jan':
-            $month = '01';
-            break;
-        
-        case 'Feb':
-            $month = '02';
-            break;
-        
-        case 'Mar':
-            $month = '03';
-            break;
-        
-        case 'Apr':
-            $month = '04';
-            break;
-        
-        case 'May':
-            $month = '05';
-            break;
-        
-        case 'Jun':
-            $month = '06';
-            break;
-        
-        case 'Jul':
-            $month = '07';
-            break;
-        
-        case 'Aug':
-            $month = '08';
-            break;
-        
-        case 'Sep':
-            $month = '09';
-            break;
-        
-        case 'Oct':
-            $month = '10';
-            break;
-        
-        case 'Nov':
-            $month = '11';
-            break;
-        
-        case 'Dec':
-            $month = '12';
-            break;
-    }
-    $year = substr($date, 8, 2);
-
-    if($year <= 14){
-        $output = '20' . $year;
-    } else {
-        $output = '19' . $year;
-    }
-
-    $output = $output . '-' . $month . '-' . substr($date, 4, 2);
-    return $output;
-}
-
-function solution3 ($date) {
-    $output = substr($date, 6, 4). '-' . substr($date, 3, 2) . '-' . substr($date, 0, 2);
-    return $output;
-}
-
-function solution4 ($date) {
-    $year = substr($date, 6, 2);
-    $output = '-' . substr($date, 0, 2) . '-' . substr($date, 3, 2);
-    //trim($output);
-    if($year <= 14){
-        $output = '20' . $year . $output;
-    } else {
-        $output = '19' . $year . $output;
-    }
-    
-    return $output;
-}
-
-$input = fopen(realpath("E:\Jobs\php-challenges\junior\iso6801\date-input.txt"), "r") or die ("Unable to open date-input.txt");
-$output = fopen("E:\Jobs\php-challenges\junior\iso6801\date-output.txt", "w") or die ("Unable to open date-input.txt");
+$input = fopen(realpath("date-input.txt"), "r") or die ("Unable to open date-input.txt");
+$output = fopen("date-output.txt", "w") or die ("Unable to open date-input.txt");
 
 while (!feof($input)) {
     $line = fgets($input);
-    
-    switch ($line) {
 
-        case (strpos($line, '#') !== false):
-          	$date = solution1($line);
-            break;
-            
-        case (strpos($line, ' ') !== false):
-            $date = solution2($line);
-            break;
-            
-        case (strpos($line, '*') !== false):
-            $date = solution3($line);
-            break;
-        
-        case (strpos($line, '-') !== false):
-          	$date = trim($line);
-            break;
-
-        case (strpos($line, '/') !== false):
-            $date = solution4($line);
-            break;
-    } 
-    fwrite($output, $date . PHP_EOL);
+    if (strpos($line, '#') !== false){
+        $line = preg_replace("/(\d+)#(\d+)#(\d+)/", "$2-$1-$3", $line);
+    }else{
+        $line = str_replace('*', '-', $line);
+    }
+    /* Part of the original note of strtotime function 
+    "If the year falls in the range 0 (inclusive) to 69 (inclusive), 2000 is added. 
+    If the year falls in the range 70 (inclusive) to 99 (inclusive) then 1900 is added." */
+    $line = date("Y-m-d", strtotime($line)); 
+    fwrite($output, $line . PHP_EOL);
 }
 
 fclose($input);
